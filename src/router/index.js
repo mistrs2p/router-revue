b5201028475ac9bb3429afba652b5d3995c6c263
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-02 21:24:52 +0430

    Commented All Unused Routes

--- src/router/index.js
+++ src/router/index.js
@@ -8,26 +8,26 @@ const routes = [
     component: Home,
   },
   
-  {
-    path: "/hawaii",
-    name: "Hawaii",
-    component: () => import( /* webpackChunkName: "hawaii" */ "../views/Hawaii"),
-  },
-  {
-    path: "/brazil",
-    name: "Brazil",
-    component: () => import( /* webpackChunkName: "brazil" */ "../views/Brazil"),
-  },
-  {
-    path: "/panama",
-    name: "Panama",
-    component: () => import( /* webpackChunkName: "panama" */ "../views/Panama"),
-  },
-  {
-    path: "/jamaica",
-    name: "Jamaica",
-    component: () => import( /* webpackChunkName: "jamaica" */ "../views/Jamaica"),
-  },
+  // {
+  //   path: "/hawaii",
+  //   name: "Hawaii",
+  //   component: () => import( /* webpackChunkName: "hawaii" */ "../views/Hawaii"),
+  // },
+  // {
+  //   path: "/brazil",
+  //   name: "Brazil",
+  //   component: () => import( /* webpackChunkName: "brazil" */ "../views/Brazil"),
+  // },
+  // {
+  //   path: "/panama",
+  //   name: "Panama",
+  //   component: () => import( /* webpackChunkName: "panama" */ "../views/Panama"),
+  // },
+  // {
+  //   path: "/jamaica",
+  //   name: "Jamaica",
+  //   component: () => import( /* webpackChunkName: "jamaica" */ "../views/Jamaica"),
+  // },
   {
     path: "/details/:id",
     name: "DestinationDetails",
