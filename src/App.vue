c22115245ac2c7fd02170c28838addaacd54915e
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-02 17:20:08 +0430

    Images Added To Assets

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
+    <a href="/">A tag</a> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
