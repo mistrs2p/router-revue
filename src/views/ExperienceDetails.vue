b0d3cbd13d3b968c149b58e38ac78064b7c0f765
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-03 03:14:17 +0430

    Experience Details Added

---
+++ src/views/ExperienceDetails.vue
@@ -0,0 +1,54 @@
+<template>
+  <section>
+    <h1>Experiences</h1>
+    <h2>{{ experience.name }}</h2>
+    <div class="experience-details">
+      <img :src="require('@/assets/' + experience.name)" :alt="experience.slug">
+      <p>{{ experience.description }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+import store from '@/store'
+export default {
+  props: {
+    slug: {
+      type: String,
+      required: true
+    },
+    experienceSlug: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    destination() {
+      return store.destinations.find(
+        destination => destination.slug == this.slug
+      )
+    },
+    experience() {
+      return this.destination.experiences.find(
+        experience => experience.slug == this.experienceSlug
+      )
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  .experience-details {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  p {
+    margin: 0 40px;
+    font-size: 20px;
+    text-align: left;
+  }
+
+  
+</style>
