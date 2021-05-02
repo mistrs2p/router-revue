73028530f28d1d2411b75b622d2903a038f80549
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-02 20:05:14 +0430

    Detail Page Configured & Store Added To Match Id

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,16 +1,43 @@
 <template>
-  <div>
-    <h1>Hello Details</h1>
-    <p>The Destination IS: {{ $route.params.id }}</p>
-  </div>
+  <section class="destination">
+    <h1>{{destination.name}}</h1>
+    <div class="destination-details">
+      <img 
+        :src="require('@/assets/' + destination.image)" 
+        :alt="destination.name"
+      >
+    </div>
+  </section>
 </template>
 
 <script>
+import store from '@/store.js'
 export default {
-
+  name: "DestinationDetails",
+  computed: {
+    destination() {
+      return store.destinations.find(
+        destination => destination.id == this.$route.params.id
+      )
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  img {
+    max-width: 600px;
+    height: auto;
+    width: 100%;
+    max-height: 400px;
+  }
+  .destination-details {
+    display: flex;
+    justify-content: space-between;
+  }
+  p {
+    margin: 0 40px;
+    font-size: 20px;
+    text-align: left;
+  }
 </style>
