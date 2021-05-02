09299a6049a7f2ff4dd404d9276124904a188f43
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-02 18:04:07 +0430

    LazyLoad Route Configured

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
-import Brazil from "../views/Brazil.vue";
-import Hawaii from "../views/Hawaii.vue";
-import Jamaica from "../views/Jamaica.vue";
-import Panama from "../views/Panama.vue";
 
 const routes = [
   {
@@ -15,22 +11,22 @@ const routes = [
   {
     path: "/hawaii",
     name: "Hawaii",
-    component: Hawaii,
+    component: () => import("../views/Hawaii"),
   },
   {
     path: "/brazil",
     name: "Brazil",
-    component: Brazil,
+    component: () => import("../views/Brazil"),
   },
   {
     path: "/panama",
     name: "Panama",
-    component: Panama,
+    component: () => import("../views/Panama"),
   },
   {
     path: "/jamaica",
     name: "Jamaica",
-    component: Jamaica,
+    component: () => import("../views/Jamaica"),
   },
 ];
 
