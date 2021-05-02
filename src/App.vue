86f270a315ca02cfd0c4a17b3de0da1d9deb4b9a
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-02 18:15:42 +0430

    Import Nav Component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/brazil">Brazil</router-link> |
-    <router-link to="/hawaii">Hawaii</router-link> |
-    <router-link to="/jamaica">Jamaica</router-link> |
-    <router-link to="/panama">Panama</router-link> 
-  </div>
+  <TheNavigation />
   <router-view />
 </template>
 
+<script>
+import TheNavigation from '@/components/TheNavigation'
+export default {
+  name: "App",
+  components: {
+    TheNavigation
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
