0f17cad23ca0b8268b3843ce66bafebb97ce93f9
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-02 17:49:04 +0430

    Store.js File Added And Loop Through It With Link In Hom.Vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="home">
+    <div 
+      v-for="destination of destinations" 
+      :key="destination.id"
+    >
+      <router-link :to="destination.slug">
+        <h2>{{ destination.name }}</h2>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import store from '@/store.js'
 export default {
   name: "Home",
   components: {
   },
+  data() {
+    return {
+      destinations: store.destinations
+    }
+  }
 };
 </script>
