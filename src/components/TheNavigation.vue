ca6487e86334d9b640108e4fb8ad0c809c9c642e
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-02 18:13:28 +0430

    TheNavigation Comoonent Added & Scaffolded & Styled

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav id="nav">
+    <router-link to="/home">Home</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/panama">Panama</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "TheNavigation"
+}
+</script>
+
+<style scoped>
+  #nav {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
