f9ce1d78641e30097a55be0668f64aa8ee2681b8
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-02 17:24:53 +0430

    Path Root Addded

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,7 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   {
-    path: "/",
+    path: "/Brazil",
     name: "Brazil",
     component: Brazil,
   },
