70ebf9e7ba6a2affe26b4b04de1e6fe65cf4e6d3
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-04 16:30:46 +0430

    Go Back Component Added

---
+++ src/components/GoBack.vue
@@ -0,0 +1,16 @@
+<template>
+  <span class="go-back">
+    <button @click="$router.go(-1)">
+      Go Back
+    </button>
+  </span>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
