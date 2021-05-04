98c0477dbd9a9c6054a1a83fa4fb36fa7bab9f84
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-04 17:19:30 +0430

    NotFound Page Added

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Bot Found</h1>
+  <p>Oops, We couldn't find that page. Try going
+    <router-link :to="{name: 'Home'}">Home</router-link>
+  </p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
