a90165fe8d83846ae311bb38db398bbc2c6f7c1a
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-04 16:37:17 +0430

    GoBack Btn Imperted To DestinationDetails

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <GoBack />
     <section class="destination">
       <h1>{{destination.name}}</h1>
       <div class="destination-details">
@@ -43,10 +44,14 @@
 
 <script>
 import store from '@/store.js'
+import GoBack from '@/components/GoBack.vue'
 export default {
   name: "DestinationDetails",
   data() {
     return {}
+  },
+  components: {
+    GoBack
   },
   // props: {
   //   slug: {
