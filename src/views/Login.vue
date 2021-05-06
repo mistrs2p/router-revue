76585eb81e561ee0eb5adab2b4657d2b9cd7b706
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-06 05:38:56 +0430

    Login Form Page Added

---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="login">
+    <h1>Login</h1>
+    <div class="form">
+      <label for="username" Username></label>
+      <input v-model="username" type="text" name="username" class="input" id="username">
+      <label for="password" Username></label>
+      <input v-model="password" type="password" name="password" class="input" id="passwoed">
+      <button @click="login" class="btn">Login</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: null,
+      password: null
+    }
+  },
+  methods: {
+    login() {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
