8449be9b37b19c98a735421f41c0f5eb6e995d12
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-06 05:56:40 +0430

    User Page Set

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,9 +2,9 @@
   <div class="login">
     <h1>Login</h1>
     <div class="form">
-      <label for="username" Username></label>
+      <label for="username">Username</label>
       <input v-model="username" type="text" name="username" class="input" id="username">
-      <label for="password" Username></label>
+      <label for="password">Password</label>
       <input v-model="password" type="password" name="password" class="input" id="passwoed">
       <button @click="login" class="btn">Login</button>
     </div>
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,9 +1,20 @@
 <template>
-  <h1>User</h1>
+  <div>
+    <h1>User</h1>
+    <button @click="logOut" class="btn">Logout</button>
+  </div>
 </template>
 
 <script>
-export default {};
+import store from '@/store'
+export default {
+  methods: {
+    logOut() {
+      store.user = null
+      this.$router.push('/')
+    }
+  }
+};
 </script>
 
 <style></style>
